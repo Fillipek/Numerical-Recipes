179aa6c8dac7e42cc7c9ed94172f80083ac006c6
Marcin Filipek <mar.lim.420@onet.pl>
2020-05-06 14:13:02 +0200

    Created a method template

--- lab_10/main.c
+++ lab_10/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// finds the minimal value of function f in range [x1, x2] with given precision e
+double findFunMin(double x1, double x2, double e, double(*f)(double));
+
 int main()
 {
-    
+
+    return 0;
+}
+
+double findFunMin(double x1, double x2, double e, double (*f)(double)) {
     return 0;
 }
